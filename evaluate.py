178f0208f04a3e8ef81b544a53d0c0e36e617325
ArgonOutPlay <argonoutplay@gmail.com>
2025-10-27 16:29:58 +0100

    evaluation script now work with new refactored rag

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,13 @@
 #env
 import os
 from dotenv import load_dotenv
+#load config (.env)
+load_dotenv() #have to be called before config import
+#semant app - RAG
+from semant_demo.config import config
+from semant_demo.weaviate_search import WeaviateSearch
+from semant_demo.rag.rag_generator import RagGenerator
+from semant_demo.schemas import RagConfig, RagSearch, SearchType
 #ragas openai
 from langchain_openai import ChatOpenAI
 from ragas.embeddings import OpenAIEmbeddings
@@ -24,12 +31,10 @@ from ragas.metrics import (
     answer_correctness
 )
 #backend 
-import httpx
 import json
 import asyncio
-import httpx
 #others
-from typing import List, Dict, Tuple
+from typing import Tuple
 import argparse
 
 #colors for better logs in terminal
@@ -39,59 +44,33 @@ class Colors:
     RED = '\033[91m'
     RESET = '\033[0m'
 
-#main class calling backend api
-class RAGAPI:
-    def __init__(self, backend_url):
-        self.backend_url = backend_url
-        self.search_result = None
-        self.client = httpx.AsyncClient()
-
-    #call weaviate search (/api/search)
-    async def search(self, query: str, limit: int = 5) -> list:
-        print(f"--- Calling /api/search: '{query}' ---")
-
-        request_body = {
-            "query": query,
-            "limit": limit,
-            #TODO: change while merged with search refactorization branch
-            "search_title_generate": False,
-            "search_summary_generate": False
-            # "type": "hybrid",
-            # "hybrid_search_alpha": 0.5
+# function that return parameters in required schemes
+def createRagSupportParameters(model_name: str, question: str, search_type: str, alpha: float, limit: int, temperature: float = 0.0):
+    #configuration of the model - api, model, temperature
+        model_config = RagConfig(
+            model_name=model_name,
+            temperature=temperature
+        )
+        # create search query, will by used to create weaviate search query
+        rag_search = RagSearch(
+            search_query = question,
+            limit= limit,
+            search_type = search_type, #'hybrid', #vector: alpha=1, text: alpha=0
+            alpha= alpha,
+            min_year= None,
+            max_year= None,
+            min_date= None,
+            max_date=None,
+            language= None
+        )
+        return {
+            "ragConfig": model_config,
+            "ragSearch": rag_search
         }
-
-        #request
-        response = await self.client.post(f"{self.backend_url}/api/search", json=request_body, timeout=60.0)
-        response.raise_for_status()
-        self.search_response = response.json()
-
-        print("--- Found:", len(self.search_response.get('results', [])), "chunks ---")
-        return self.search_response.get('results', [])
-    
-    #call rag (/api/rag)
-    async def call_rag(self, query: str, model_name: str) -> str:
-        if not self.search_response:
-            raise ValueError("Search must be called before rag")
         
-        print(f"--- Calling /api/rag: '{query}' ---")
-
-        request_body = {
-            "search_response": self.search_response,
-            "question": query,
-            "history": [],
-            "model_name": model_name
-        }
-        #request
-        response = await self.client.post(f"{self.backend_url}/api/rag", json=request_body, timeout=60.0)
-        response.raise_for_status()
-        answer = response.json()
-
-        print("--- RAG API processed succesfuly. ---")
-
-        return answer.get('rag_answer', 'ERROR no: "rag_answer".')
 
 #load questions/queries and ground truths from json file given path
-def loadDataFromJson (path: str) -> Tuple[List[str], List[str]]:
+def loadDataFromJson (path: str) -> Tuple[list[str], list[str]]:
     queries = []
     gts = []
     try:
@@ -112,7 +91,7 @@ def loadDataFromJson (path: str) -> Tuple[List[str], List[str]]:
 
 
 #load questions/queries from txt file given path
-def loadDataFromTXT(path: str) -> List[str]:
+def loadDataFromTXT(path: str) -> list[str]:
     result = []
     try:
         with open(path, 'r', encoding='utf-8') as f:
@@ -132,8 +111,6 @@ def loadDataFromTXT(path: str) -> List[str]:
         
 
 async def main():
-    #load config (.env)
-    load_dotenv() 
     #get required variables
     parser = argparse.ArgumentParser(description="Evaluator for RAG.")
     parser.add_argument("--mode",
@@ -146,6 +123,23 @@ async def main():
                         default="OLLAMA",
                         choices=["OLLAMA","GOOGLE", "OPENAI"],
                         help="Model used by RAG: 'OLLAMA', 'GOOGLE' or 'OPENAI' ")
+    parser.add_argument("--temperature",
+                    type=float,
+                    default=0.0,
+                    help="RAG model temperature.")
+    parser.add_argument("--search_type",
+                    type=str,
+                    default="hybrid",
+                    choices=["hybrid","vector", "text"],
+                    help="Search type used for searching in DB.")
+    parser.add_argument("--alpha",
+                    type=float,
+                    default=0.5,
+                    help="Hybrid search alpha parametr.")
+    parser.add_argument("--limit",
+                    type=int,
+                    default=5,
+                    help="Limit of chunks that will be retrieved during db search.")
     parser.add_argument("--eval_model",
                     type=str,
                     default="OLLAMA",
@@ -168,7 +162,13 @@ async def main():
 
     args = parser.parse_args()
 
-    print(f"{Colors.GREEN} Starting evaluation in mode: {args.mode} with RAG model: {args.rag_model} and evaluation model: {args.eval_model} and precission: {args.precission} and synthetic dataset: {args.synthetic_dataset}. {Colors.RESET} ")
+    print(f"""{Colors.GREEN} 
+            Starting evaluation in mode: {args.mode} with RAG model: {args.rag_model}
+            and temperature: {args.temperature} and limit: {args.limit}
+            and search type: {args.search_type} and alpha parametr: {args.alpha} 
+            and evaluation model: {args.eval_model} and precission: {args.precission} 
+            and synthetic dataset: {args.synthetic_dataset}. 
+            {Colors.RESET} """)
 
     eval_model = args.eval_model
     rag_model = args.rag_model
@@ -220,11 +220,11 @@ async def main():
         print(f"\n Invalid model: {eval_model}. Possible models: [OPENAI, OLLAMA].")
         return
 
-    print(f"--- Model used for evaluation is {eval_model} precisely: {eval_model_name} ---")
+    print(f"{Colors.YELLOW} --- Model used for evaluation is {eval_model} precisely: {eval_model_name} --- {Colors.RESET}")
     
     #--- evaluation ---
-    #rag api class
-    rag_api = RAGAPI(os.getenv("BACKEND_API_URL"))
+    search = await WeaviateSearch.create(config=config)
+    rag_generator = RagGenerator(config=config, search=search)
     try:
         dataset = []
         if (mode == "NOGT"):
@@ -232,23 +232,36 @@ async def main():
             precisions = []
 
             for query in queries:
-                #search
-                retrieved_context  = await rag_api.search(query, limit=os.getenv("CHUNK_LIMIT") )
-                retrieved_contexts_text = [ctx['text'] for ctx in retrieved_context]
-                #rag
-                rag_answer = await rag_api.call_rag(query, rag_model)
+                ragSP = createRagSupportParameters(
+                    model_name=rag_model,
+                    question=query,
+                    search_type=args.search_type,
+                    alpha=args.alpha,
+                    limit=args.limit,
+                    temperature=args.temperature
+                    
+                )
+                ragResult = await rag_generator.generate_answer(
+                    question_string = query,
+                    history= [],
+                    #rag configuration parameters
+                    rag_config = ragSP["ragConfig"],
+                    #search parameters
+                    rag_search = ragSP["ragSearch"]
+                )
+                retrieved_contexts_text = [ctx.text for ctx in ragResult["sources"]]
                 dataset.append(
                     {
-                        "user_input":query,
-                        "retrieved_contexts":retrieved_contexts_text,
-                        "response":rag_answer
+                        "user_input" : query,
+                        "retrieved_contexts" : retrieved_contexts_text,
+                        "response" : ragResult["answer"]
                     }
                 )
                 #calculating precission (current eval require GT to be able to calculate precission)
                 if(precission_mode == True):
                     sample = SingleTurnSample(
                         user_input=query,
-                        response=rag_answer,
+                        response=ragResult["answer"],
                         retrieved_contexts=retrieved_contexts_text
                     )
                     #add precision of sample to list
@@ -256,54 +269,64 @@ async def main():
 
             evaluation_dataset = EvaluationDataset.from_list(dataset)
 
-            print(f"---Starting evaluation ---")
+            print(f"{Colors.YELLOW} ---Starting evaluation --- {Colors.RESET}")
 
             result = evaluate(dataset=evaluation_dataset,
                               metrics=[faithfulness, answer_relevancy],
                               llm=llm)
             
             print(f"{Colors.GREEN}---Evaluation finished ---{Colors.RESET}")
-            print(result)
+            print(f"{Colors.GREEN} {result} {Colors.RESET}")
             if(precission_mode == True):
                 average_precision = sum(precisions) / len(precisions)
-                print(f"average_precission: {average_precision}")
+                print(f"{Colors.GREEN} average_precission: {average_precision} {Colors.RESET}")
 
         elif(mode == "GT"):
             for query, gt in zip(queries, ground_truths):
-                #search
-                retrieved_context  = await rag_api.search(query, limit=os.getenv("CHUNK_LIMIT") )
-                retrieved_contexts_text = [ctx['text'] for ctx in retrieved_context]
-                #rag
-                rag_answer = await rag_api.call_rag(query, rag_model)
+                ragSP = createRagSupportParameters(
+                    model_name=rag_model,
+                    question=query,
+                    search_type=args.search_type,
+                    alpha=args.alpha,
+                    limit=args.limit,
+                    temperature=args.temperature
+                    
+                )
+                ragResult = await rag_generator.generate_answer(
+                    question_string = query,
+                    history= [],
+                    #rag configuration parameters
+                    rag_config = ragSP["ragConfig"],
+                    #search parameters
+                    rag_search = ragSP["ragSearch"]
+                )
+                retrieved_contexts_text = [ctx.text for ctx in ragResult["sources"]]
                 dataset.append(
                     {
                         "user_input":query,
                         "retrieved_contexts":retrieved_contexts_text,
-                        "response":rag_answer,
+                        "response":ragResult["answer"],
                         "reference": gt
                     }
                 )
             evaluation_dataset = EvaluationDataset.from_list(dataset)
 
-            print(f"---Starting evaluation ---")
+            print(f"{Colors.YELLOW} ---Starting evaluation --- {Colors.RESET}")
 
             result = evaluate(dataset=evaluation_dataset,
                               metrics=[faithfulness, answer_relevancy, context_recall, context_precision, answer_correctness],
                               llm=llm)
             
             print(f"{Colors.GREEN}---Evaluation finished ---{Colors.RESET}")
-            print(result)
+            print(f"{Colors.GREEN} {result} {Colors.RESET}")
         else:
-            print(f"\n Invalid mode: {mode}. Possible modes: [GT, NOGT].")
+            print(f"{Colors.RED}Invalid mode: {mode}. Possible modes: [GT, NOGT].{Colors.RESET}\n")
 
-    except httpx.RequestError as e:
-        print(f"\nRAG EVALUATION ERROR: Can not connect to backend on url: {os.getenv("BACKEND_API_URL")}..")
-    except httpx.HTTPStatusError as e:
-        print(f"\nRAG EVALUATION ERROR: API error code: {e.response.status_code}.")
-        print("Error detail:", e.response.text)
+    except Exception as e:
+        print(f"{Colors.RED} Error detail: {e} {Colors.RESET}\n")
     finally:
-        if 'rag_api' in locals():
-            await rag_api.client.aclose()
+        if 'search' in locals():
+            await search.close()
 
 
 if __name__ == "__main__":
